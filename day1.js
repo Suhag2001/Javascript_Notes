8f721868cc2a20ca3c7ba377057b2edf69e3e04e
Suhag Mohinkar <113937289+Suhag2001@users.noreply.github.com>
2023-08-13 22:45:12 -0700

    first commit

---
+++ day1.js
@@ -0,0 +1,77 @@
+// console.log("hello coder");
+
+
+// there are 8 types of data types in javascript 
+
+
+// // 1.Number 
+// let age = 20 ;
+// console.log(typeof age);
+// // 2.String 
+// let name =  "pallavi";
+// console.log(typeof name);
+// // 3.Boolean 
+// let isStudent =  true ;
+// console.log(typeof isStudent);
+// // 4.Underfined 
+// let isUndefined;
+// console.log(typeof isUndefined);
+// // 5.Null
+// let isNull= null ;
+// console.log(typeof  isNull);
+// // 6.Symbol
+// let isSymbol = Symbol("declare");
+// console.log(typeof isSymbol);
+// // 7.Object
+// let isObject ={
+//     "name":"aman",
+//     "age":21
+// }
+// console.log(typeof isObject);
+// // 8.BigInt
+// let isBigInt = 49n;
+// console.log(typeof isBigInt);
+
+// let a = "ten" ;
+// let b =10;
+
+// if(a===b){
+//     console.log("is equal");
+// }
+
+// string to number conversion
+// 1.Number
+// 2.parseInt 
+// 3.parseFloat
+
+// let a = "1000.023f"
+
+// console.log( typeof Number(a));
+// console.log(Number(a));
+// console.log(parseInt(a));
+// console.log(parseFloat(a));
+// console.log(typeof Infinity);
+// console.log(typeof NaN); //Not a Number
+
+// //Math
+
+// 1.Math.round
+// 2.Math.ceil
+// 3.Math.floor
+
+let a = 123.1376;
+let b = 124.67;
+
+// console.log(Math.round(a));//0-4
+// console.log(Math.round(b));//5-9
+
+console.log(Math.floor(a));//neglate
+console.log(Math.floor(b));//neglate
+
+// console.log(Math.ceil(a));//1-9
+// console.log(Math.ceil(b));//1-9
+
+//toFixed
+
+console.log(a.toFixed(0));
+console.log(a.toFixed(2));
---
+++ day2.js
@@ -0,0 +1,184 @@
+//Comments 
+//There are two types of comments 
+// 1.Single line comment 
+// sinle line comment 
+// 2.Multi line comment
+/* multi 
+line 
+comments 
+*/ 
+
+
+// Operators
+// 1.Arithmetic Operators 
+// + 
+// -
+// /
+// *
+// %
+// ++
+// --
+
+// 2. Comparison Operators
+// ==
+// ===
+// != 
+// >     
+// >=
+// <
+// <=
+
+// 3.Logical Operator 
+
+// &&
+// var a = 25 , b=20 , c=30 ;
+// if(a>b && a>c){
+//     console.log(" a is greater");
+// }
+// ||
+// if(a>b ||a>c){
+//     console.log(" a is greater");
+// }
+// !
+// if( a != b){
+//     console.log("a is not equal to b");
+// }
+
+// 4.Assignment operator
+
+// =
+// var a =10 ;
+// console.log(a);
+// +=  , a = a+10 
+// a += 10;
+// console.log(a);
+
+// -=
+// a -= 5;
+// console.log(a);
+// /=
+// a /= 2 ;
+// console.log(a);
+// *=
+// a *= 2 ;
+// console.log(a);
+// %=
+// a %= 2 ;
+// console.log(a);
+
+//Conditional statement 
+// 1. if 
+// if(condition){
+//     // code 
+// }
+// var a = 10 , b= 20 ;
+// if(a < b){
+//     console.log( a+ " is small");
+// }
+// 2 . if else 
+// var a = 10 , b= 20 ;
+// if(a > b){
+//     console.log( a+ " is small");
+// }else{
+//     console.log( b + "is greater");
+// }
+// 3 . Nested if ( if else if )
+// var a = 10 , b= 40 , c=30;
+// if(a > b && a > c){
+//     console.log( a+ " is greater");
+// }else if(b> a && b> c){
+//     console.log( b + "is greater");
+// }else{
+//     console.log(c + "is greater");
+// }
+
+// var op = "*";
+// var a = 10 , b= 20 ;
+
+// if (op=="+"){
+//     console.log( "add =" + (a+b));
+// }else if (op=="-"){
+//     console.log( "sub =" + (a-b));
+// }else if (op=="/"){
+//     console.log( "div =" + (a/b));
+// }else if (op=="*"){
+//     console.log( "mul =" + (a*b));
+// }else{
+//     console.log("invalid operation");
+// }
+// 4. switch case
+
+
+// var op = "/";
+// var a = 40 , b= 20 ;
+// switch(op){
+//     case "+":{
+//         console.log( "add =" + (a+b));
+//         break ;
+//     }
+//     case "-":{
+//         console.log( "sub =" + (a-b));
+//         break ;
+//     }
+//     case "*":{
+//         console.log( "sub =" + (a*b));
+//         break ;
+//     }
+//     case "/":{
+//         console.log( "div =" + (a/b));
+//         break ;
+//     }
+//     default:{
+//         console.log("invalid operation");
+//     }
+// }
+
+// Loop
+// 1. for 
+// for( intialization ; condition ; increament / decreament ){
+//     //code ;
+// }
+
+// for(var a = 1 ; a<=10 ; a++){
+//     console.log(a);
+// }
+
+// for(var a = 1 ; a<=3 ; a++){
+//    console.log("outer"+a);
+//    for(var b = 1 ; b<=3 ; b++){
+//     console.log("inner" +b);
+//    }
+// }
+
+// 1234
+// 1234
+// 1234
+
+// for(var i = 1 ; i<=3; i++){
+//     var a = "";
+//     for(var j = 1 ; j<=4 ;j++){
+//       a += j;
+//     }
+//     console.log(a);
+// }
+
+for(var i = 1 ; i<=10 ;i++){
+    if(i==5){
+        continue;
+    }
+    console.log(i);
+}
+
+// 2. while 
+// var i = 1 ;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// 3. do while
+// var a =11 ;
+// do{
+// console.log(a);
+// a++;
+// }while(a<=10)
