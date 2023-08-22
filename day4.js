d643d6858d44f8f809d98baf4c6cc62c66c22da6
= <=>
2023-08-22 12:09:24 +0530

    day 4

---
+++ day4.js
@@ -0,0 +1,45 @@
+//String
+
+//6. String search methods
+
+let n = "I am a Developer, I am a Student";
+
+// indexOf() -
+// console.log(n.indexOf("Developer"));
+// console.log(n.indexOf("am",1));  //index of take the searching index argument
+// lastIndexOf()
+// console.log(n.lastIndexOf("am"));
+// search()
+// console.log(n.search("D"));
+// console.log(n.search(/Am/i));
+// includes()
+// console.log(n.includes("z"));
+// startWith()
+// console.log(n.startsWith("i"));
+// endWith()
+// console.log(n.endsWith("I"));
+
+// 7. String Template / Template Literals / Back-tick(``)
+
+// Quotes inside string 
+// let  a = `I'm a "Developer"`;
+// console.log(a);
+
+//multiline string 
+
+// let a = `I am a Developer
+//   I am a Student 
+//   I am learning js`;
+// console.log(a);    
+//Interpolation (${var})
+
+// let a = "JavaScript";
+// console.log(` I am learning ${a} `);
+// console.log(` addition = ${2+4} `);
+
+// let a = "aman  pallavi prachi";
+// console.log(a.split(" "));
+
+let a ="                  I am  a Developer         ";
+console.log(a);
+console.log(a.trim());
