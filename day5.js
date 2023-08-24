2cb99a6a97424bf8e5c841647baaca5b681c8fdd
= <=>
2023-08-24 12:31:33 +0530

    day 5

---
+++ day5.js
@@ -0,0 +1,53 @@
+// Array
+
+let a = {name:"Pallavi",city:"Nagpur"};
+
+let arr= [26,"Pallavi", true,["Nagpur","Pune",45],a];
+
+// console.log(arr);
+// console.log(typeof arr);
+
+// console.log(typeof arr[0]);
+// console.log(typeof arr[1]);
+// console.log(typeof arr[2]);
+// console.log(typeof arr[3]);
+// console.log(typeof arr[4]);
+
+// console.log(arr.length);
+
+// arr[arr.length]="Aman";  //adding
+
+// arr[0]= 55;  //replacing
+
+// delete arr[1]; //deleting
+
+// Forget the above method for add , replace and delete the data 
+
+// push()
+// arr.push("Prachi");
+// pop()
+// let n =arr.pop();
+// unshift()
+// arr.unshift("Bootcoding");
+
+// shift()
+
+//  let n= arr.shift();
+// console.log(n);
+
+// splice(index, deleteCount);
+
+// arr.splice(1,4);
+
+// arr.splice(1,1,"Prachi",23,"bootcoding");
+
+// console.log(arr);
+
+//includes()
+
+// console.log(arr.includes("prachi"));
+
+// let arr=["a","c","q","e","td","h","z","f","a","a"];
+
+console.log(arr);
+// console.log(arr.sort());
